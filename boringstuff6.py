93f847a10c832e13391fde322444d2ec6f8e9f0c
grace <58996074+glandsberg@users.noreply.github.com>
2020-11-05 23:04:01 -0500

    Exercises done as of 5 November 2020
    mostly pyperclip module exercises

---
+++ boringstuff6.py
@@ -0,0 +1,3 @@
+pip install pyperclip
+pyperclip.copy('Hello world!')
+pyperclip.paste()
---
+++ table-printer.py
@@ -0,0 +1,10 @@
+# table-printer.py
+
+# want to print a table
+
+
+def printTable(outer_lists):
+    widths = []
+    for idx, inner_list in outer_lists:
+        max width =
+# unfinished
