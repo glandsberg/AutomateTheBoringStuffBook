ee51b4af3c3572de61bac317e309313e92ddee0f
grace <58996074+glandsberg@users.noreply.github.com>
2021-12-08 21:08:30 -0500

    Add files via upload
    "Say you have a spreadsheet of data from the 2010 US Census and you
    have the boring task of going through its thousands of rows to count both
    the total population and the number of census tracts for each county"
    
    Project from Chapter 12 of Automate the Boring Stuff

--- readCensusExcel.py
+++ readCensusExcel.py
@@ -4,33 +4,43 @@
 import openpyxl, pprint
 print('Opening workbook...')
 wb = openpyxl.load_workbook('censuspopdata.xlsx')
-sheet = wb['Population by Census Tract']
+sheet = wb.get_sheet_by_name('Population by Census Tract')
 countyData = {}
 
-# TODO: Fill in CountyData with each county's population and tracts.
-
+#TODO: Fill in countyData with each county's population and tracts.
 print('Reading rows...')
-
-for row in range(2, sheet.max_row + 1):
+for row in range(2, sheet.get_highest_row() + 1):
     # Each row in the spreadsheet has data for one census tract.
     state = sheet['B' + str(row)].value
     county = sheet['C' + str(row)].value
-    pop = sheet['D' + str(row)].value
+    pop = sheet['D' + str(row)].value 
 
-# TODO: Open a new text file and write the contents of countyData to it
+#TODO: Open a new text file and write the contents of countyData to it.
+
+# The countyData dictionary key will look like:
+# countyData[state abbrev][county]['tracts']
+
+for row in range(2, sheet.get_highest_row() +1):
+    # Each row in spreadsheet has data for one census tract.
+    state = sheet['B' + str(row)].value
+    county = sheet ['C' + str(row)].value
+    pop = sheet['D' + str(row)].value
 
 # Make sure the key for this state exists.
     countyData.setdefault(state, {})
-        # Make sure the key for this county in this state exists
+# Make sure the key for the county in this state exists.
     countyData[state].setdefault(county, {'tracts': 0, 'pop': 0})
 
-        # Each row represents one census tract, so increment by one
+# Each row represents one census tract, so increment by one
     countyData[state][county]['tracts'] += 1
-        # Increase the county pop by the pop in this census tract
+# Increase the county pop by the pop in this census tract.
     countyData[state][county]['pop'] += int(pop)
 
 # TODO: Open a new text file and write the contents of CountyData to it
 
+for row in range(2, sheet.get_highest_row() +1):
+
+# Open a new text file and write the contents of countyData to it.
 print('Writing results...')
 resultFile = open('census2010.py', 'w')
 resultFile.write('allData = ' + pprint.pformat(countyData))
